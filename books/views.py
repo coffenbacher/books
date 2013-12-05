987c22caec2450081da040ba82f5210ca4e85015
Charles Offenbacher <charles.offenbacher@gmail.com>
2013-12-04 22:33:25 -0800

    login required, cleaning up extra view

--- books/views.py
+++ books/views.py
@@ -1,13 +1,9 @@
 from books.models import *
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     books = Book.objects.all()
     return render(request, 'index.html', {'books': books})
-
-def update_book(request):
-    isbn = request.POST.get('ISBN')
-    b = Book.objects.get(isbn = isbn)
-    price = b.get_current_price()
-    return HttpResponse(price)
