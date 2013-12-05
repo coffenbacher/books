86c7bfe2522ace0bdda03a225763f58c94894cc7
Charles Offenbacher <charles.offenbacher@gmail.com>
2013-12-04 22:34:37 -0800

    fixing urls

--- books/urls.py
+++ books/urls.py
@@ -6,7 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'books.views.index', name='index'),
-    url(r'^update_book$', 'books.views.update_book', name='update_book'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
